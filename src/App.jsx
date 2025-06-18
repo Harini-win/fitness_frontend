6863efb65be4382d877d7d5a43a41c7997f7e338
Harini-win <harini.selvakummar@gmail.com>
2025-06-18 17:49:05 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ import Progress from "./progress";
 import Dance from "./dance";
 
 function App() {
- return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/home" element={<FitnessDashboard />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/progress" element={<Progress />} />
-        <Route path="/dance" element={<Dance />} />
-      </Routes>
-    </BrowserRouter>
-  );
+return (
+  <BrowserRouter basename={process.env.PUBLIC_URL || "/"}>
+    <Routes>
+      <Route path="/" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/home" element={<FitnessDashboard />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/progress" element={<Progress />} />
+      <Route path="/dance" element={<Dance />} />
+    </Routes>
+  </BrowserRouter>
+);
 }
 
 export default App;
