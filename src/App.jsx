9c79e5522847ce91a67919ca901a96f5fa351c19
Harini-win <harini.selvakummar@gmail.com>
2025-06-18 18:06:32 +0530

    one final try

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import { useState,useRef,useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginForm from "./login";
-import RegisterForm from "./register";
-import FitnessDashboard from "./home";
-import Work from "./workout";
-import Progress from "./progress";
-import Dance from "./dance";
 
-function App() {
-return (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/home" element={<FitnessDashboard />} />
-      <Route path="/work" element={<Work />} />
-      <Route path="/progress" element={<Progress />} />
-      <Route path="/dance" element={<Dance />} />
-    </Routes>
-  </BrowserRouter>
-);
-}
 
-export default App;
+
+
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./login";
+import RegisterForm from "./register";
+import FitnessDashboard from "./home";
+import Work from "./workout";
+import Progress from "./progress";
+import Dance from "./dance";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/home" element={<FitnessDashboard />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/progress" element={<Progress />} />
+      <Route path="/dance" element={<Dance />} />
+    </Routes>
+  </BrowserRouter>
+  </StrictMode>
 )
